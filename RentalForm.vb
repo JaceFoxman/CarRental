2f693f25d99c1c4f398c87c42fc9e0727c9003ad
Jason Permann <JaceFoxman@gmail.com>
2025-04-14 09:43:34 -0600

    Add User Input validation code

--- RentalForm.vb
+++ RentalForm.vb
@@ -9,6 +9,60 @@ Option Strict On
 Option Compare Binary
 
 Public Class RentalForm
+    Function UserInput() As Boolean
+        Dim valid As Boolean = True
+        Dim errorMessage As String
+        If IsNumeric(BeginOdometerTextBox.Text) = False Then
+            valid = False
+            BeginOdometerTextBox.Focus()
+            errorMessage &= "Odometer must be a numeric value!"
+        End If
+
+        If IsNumeric(EndOdometerTextBox.Text) = False Then
+            valid = False
+            EndOdometerTextBox.Focus()
+            errorMessage &= "Odometer must be a numeric value!"
+        End If
+
+        If IsNumeric(DaysTextBox.Text) = False Then
+            valid = False
+            DaysTextBox.Focus()
+            errorMessage &= "Amount of days rented must be a numeric value!"
+        End If
+
+        If NameTextBox.Text = "" Then
+            valid = False
+            errorMessage &= "Please enter the Customer's name."
+        End If
+
+        If AddressTextBox.Text = "" Then
+            valid = False
+            errorMessage &= "Please enter the Customer's adress."
+        End If
+
+        If CityTextBox.Text = "" Then
+            valid = False
+            errorMessage &= "Please enter the Customer's city."
+        End If
+
+        If StateTextBox.Text = "" Then
+            valid = False
+            errorMessage &= "Please enter the Customer's state."
+        End If
+
+        If ZipCodeTextBox.Text = "" Then
+            valid = False
+            errorMessage &= "Please enter the Customer's ZIP."
+        End If
+
+        If Not valid Then
+            'My.Computer.Audio.Play(My.Resources.KH_Select, AudioPlayMode.Background)
+            MsgBox(errorMessage, MsgBoxStyle.Critical, "Customer Information Error")
+        End If
+        Return valid
+    End Function
+
+    'Defaults and Clear______________________________________________________________________________
     Private Sub RentalForm_Load(sender As Object, e As EventArgs) Handles Me.Load
         SetDefaults()
     End Sub
@@ -27,7 +81,10 @@ Public Class RentalForm
         TotalDiscountTextBox.Text = ""
         TotalChargeTextBox.Text = ""
         SummaryButton.Enabled = False
-
+        MilesradioButton.Checked = True
+        KilometersradioButton.Checked = False
+        AAAcheckbox.Checked = False
+        Seniorcheckbox.Checked = False
     End Sub
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         Clear()
