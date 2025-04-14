7513910e4c646b653e983a148790a539b619677b
Jason Permann <JaceFoxman@gmail.com>
2025-04-14 09:12:07 -0600

    Add Default values from first load

--- RentalForm.vb
+++ RentalForm.vb
@@ -7,11 +7,28 @@
 Option Explicit On
 Option Strict On
 Option Compare Binary
-Public Class RentalForm
-    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
-        Me.Close()
-    End Sub
 
+Public Class RentalForm
+    Private Sub RentalForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SetDefaults()
+    End Sub
+    Sub SetDefaults()
+        NameTextBox.Text = ""
+        AddressTextBox.Text = ""
+        CityTextBox.Text = ""
+        StateTextBox.Text = ""
+        ZipCodeTextBox.Text = ""
+        BeginOdometerTextBox.Text = ""
+        EndOdometerTextBox.Text = ""
+        DaysTextBox.Text = ""
+        TotalMilesTextBox.Text = ""
+        MileageChargeTextBox.Text = ""
+        DayChargeTextBox.Text = ""
+        TotalDiscountTextBox.Text = ""
+        TotalChargeTextBox.Text = ""
+        SummaryButton.Enabled = False
+
+    End Sub
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         Clear()
     End Sub
@@ -24,5 +41,14 @@ Public Class RentalForm
         BeginOdometerTextBox.Text = ""
         EndOdometerTextBox.Text = ""
         DaysTextBox.Text = ""
+        TotalMilesTextBox.Text = ""
+        MileageChargeTextBox.Text = ""
+        DayChargeTextBox.Text = ""
+        TotalDiscountTextBox.Text = ""
+        TotalChargeTextBox.Text = ""
     End Sub
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
 End Class
