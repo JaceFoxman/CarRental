bd01d82e95651fceece1c0735e51383fd74a84af
Jason Permann <JaceFoxman@gmail.com>
2025-04-14 08:44:11 -0600

    Add Header

--- RentalForm.vb
+++ RentalForm.vb
@@ -1,6 +1,14 @@
-﻿Option Explicit On
+﻿'Jason Permann
+'Spring 2025
+'RCET2265
+'Car Rental
+
+
+Option Explicit On
 Option Strict On
 Option Compare Binary
 Public Class RentalForm
+    Private Sub RentalForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
 End Class
